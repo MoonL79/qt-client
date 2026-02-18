1c2ea237e649e0d4f04d56c148f2442d398a72df
MoonLL <zc15315336096@163.com>
2026-02-18 11:58:32 +0800

    登录窗口实现

--- src/ui/login/loginwindow.h
+++ src/ui/login/loginwindow.h
@@ -1,10 +1,12 @@
 #ifndef LOGINWINDOW_H
 #define LOGINWINDOW_H
 
-#include <QWidget>
-#include <QPoint>
 #include <QMouseEvent>
 #include <QPaintEvent>
+#include <QPoint>
+#include <QWidget>
+
+#include "networkclient.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -12,32 +14,31 @@ class LoginWindow;
 }
 QT_END_NAMESPACE
 
-class LoginWindow : public QWidget
-{
-    Q_OBJECT
+class LoginWindow : public QWidget {
+  Q_OBJECT
 
 public:
-    explicit LoginWindow(QWidget *parent = nullptr);
-    ~LoginWindow();
+  explicit LoginWindow(QWidget *parent = nullptr);
+  ~LoginWindow();
 
 signals:
-    void loginSuccess(const QString &username);
+  void loginSuccess(const QString &username);
 
 private slots:
-    void onLoginClicked();
-    void onRegisterClicked();
-    void onCloseClicked();
+  void onLoginClicked();
+  void onRegisterClicked();
+  void onCloseClicked();
 
 protected:
-    void mousePressEvent(QMouseEvent *event) override;
-    void mouseMoveEvent(QMouseEvent *event) override;
-    void mouseReleaseEvent(QMouseEvent *event) override;
-    void paintEvent(QPaintEvent *event) override;
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseMoveEvent(QMouseEvent *event) override;
+  void mouseReleaseEvent(QMouseEvent *event) override;
+  void paintEvent(QPaintEvent *event) override;
 
 private:
-    Ui::LoginWindow *ui;
-    QPoint m_dragPosition;
-    bool m_isDragging;
+  Ui::LoginWindow *ui;
+  QPoint m_dragPosition;
+  bool m_isDragging;
 };
 
 #endif // LOGINWINDOW_H
